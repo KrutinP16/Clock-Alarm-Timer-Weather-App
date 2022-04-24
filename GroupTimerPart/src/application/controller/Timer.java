e33e5c7461daf0b7852bd941e2f2ad18dfec0b7f
KrutinP16 <99836695+KrutinP16@users.noreply.github.com>
2022-04-24 12:48:51 -0500

    Update Timer.java

--- GroupTimerPart/src/application/controller/Timer.java
+++ GroupTimerPart/src/application/controller/Timer.java
@@ -1,5 +1,5 @@
 package application.controller;
-
+//IMPORT THE JAVA APPLICATIONS
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -9,20 +9,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
-
+//DECLARE MY CLASS
 public class Timer {
+	//DECLARE VARIABLES
 	boolean play = false;
 	int elapsedTime = 0;
 	int seconds = 0;
 	int minutes = 0;
 	int hours = 0;
-
+	
+	// CODE FOR THE FXML PARTS FOR THE TIMER
 	@FXML
 	private Label timeLabel;
 
 	@FXML
 	private VBox timer;
-
+	
+	// FXML BUTTON FOR RESET AND CONFIGURE THE CODE TO RESET TO 00:00:00
 	@FXML
 	void reset(ActionEvent event) {
 		play = false;
@@ -33,7 +36,8 @@ public class Timer {
 	public Timer() {
 		bindToTime();
 	}
-
+	
+	
 	private void bindToTime() {
 
 		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0), new EventHandler<ActionEvent>() {
@@ -41,6 +45,7 @@ public class Timer {
 			public void handle(ActionEvent actionEvent) {
 
 				if (play) {
+					//CALCULATE THE SEC MIN AND HOURS TO PLAY THE TIMER
 					elapsedTime = elapsedTime + 1000;
 					hours = (elapsedTime / 3600000);
 					minutes = (elapsedTime / 60000) % 60;
@@ -54,12 +59,12 @@ public class Timer {
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.play();
 	}
-
+	//START THE BUTTON FOR THE TIMER
 	@FXML
 	void start(ActionEvent event) {
 		play = true;
 	}
-
+	//STOP BUTTON TO PAUSE THE TIMER
 	@FXML
 	void stop(ActionEvent event) {
 		play = false;
